2cba985e923cdd883552eb975f068e31f51f7ca0
Antonio Patterson <dbmack98@gmail.com>
2014-05-07 19:01:24 -0400

    Fixed Errors !

--- adminlist.php
+++ adminlist.php
@@ -2,16 +2,16 @@
 
 /*
 __PocketMine Plugin__
-name=admins
+name=StaffList
 description=Displays current server rules.
 version=0.1.1
-author=ShaderGameing
-class=admins
-apiversion=9
+author=PHP_PRO
+class=staff
+apiversion=11,12,13
 */
 
 
-class Rules implements Plugin{
+class staff implements Plugin{
         private $api, $path;
         public function __construct(ServerAPI $api, $server = false){
                 $this->api = $api;
@@ -19,22 +19,22 @@ class Rules implements Plugin{
 
         public function init(){
           $this->api->console->register("admins", "Displays admins to user", array($this, "admins"));
-          $this->api->ban->cmdWhitelist("admins"); // Non OPs can use the command too
+          $this->api->ban->cmdWhitelist("admins"); 
           $this->path = $this->api->plugin->configPath($this);
-          $this->msgs = new Config($this->path."msgs.yml", CONFIG_YAML, array( // configurable messages list, so it's easy to add a msg, just add one in the msgs.yml file
-                        "<--[ADMINS]-->",
-                        "1. NAME",
-                        "2. NAME",
-                        "3. NAME",
-                        "4. NAME",
-						"5. NAME",
+          $this->msgs = new Config($this->path."msgs.yml", CONFIG_YAML, array( 
+                        "~~~[Current Staff ]~~~",
+                        "[RANK] NAME", // [RANK] Can be turned into EX: [Admin] [Owner] [Mod]
+                        "[RANK] NAME",
+                        "[RANK] NAME",
+                        "[RANK] NAME",
+			"[RANK] NAME",
                 ));
-                $this->msgs = $this->api->plugin->readYAML($this->path . "msgs.yml"); // so it's not in the stupid config format
+                $this->msgs = $this->api->plugin->readYAML($this->path . "msgs.yml");
         }
 
         public function Rules($cmd, $args, $issuer){
                 $username = $issuer->username;
-                foreach($this->msgs as $msg){ // for each message in the msgs.yml file , send a msg to the player
+                foreach($this->msgs as $msg){ 
                         $this->api->chat->sendTo(false, $msg, $username);
                 }
         }
